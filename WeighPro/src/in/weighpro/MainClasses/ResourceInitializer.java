e8de036f989f23586820a8ac90bd3bfa3e13c98c
emdroidery <farhanrd@gmail.com>
2014-07-14 12:03:14 +0530

    Added comments to ResourceInitializer.java
    Still have to add the implementation of the database initializer

--- WeighPro/src/in/weighpro/MainClasses/ResourceInitializer.java
+++ WeighPro/src/in/weighpro/MainClasses/ResourceInitializer.java
@@ -1,11 +1,23 @@
+/* This class initializes the
+* database connections and the
+* fonts which will be required in
+* the application
+
+* Made for WeighPro
+* Author Weis Electonics Pvt. Ltd.
+* Contribution by emdroidery
+*/
+
 package in.weighpro.MainClasses;
 
 import in.weighpro.customFonts.DigitalFontGenerator;
 import in.weighpro.customFonts.MenuFontGenerator;
 
 public class ResourceInitializer {
+	
+	//Constructor to initialize the connection and the fonts
 	public ResourceInitializer() {
-		new MenuFontGenerator();
-		new DigitalFontGenerator();
+		new MenuFontGenerator();//Class that generates the font used in the menu buttons
+		new DigitalFontGenerator();//Class that generates the font used by the weight field
 	}
 }
