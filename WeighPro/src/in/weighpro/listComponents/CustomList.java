b38f6e6d820a28c92b21c6f5abaca9c01c8ece2f
rasheedf <rasheedf@IN-HYD2-567QQ12.ant.amazon.com>
2014-07-24 18:59:57 +0530

    Added comments to CustomList.java

--- WeighPro/src/in/weighpro/listComponents/CustomList.java
+++ WeighPro/src/in/weighpro/listComponents/CustomList.java
@@ -1,3 +1,12 @@
+/*
+* This class provides the
+* pluggable list which is 
+* used in various panels 
+
+* Made for WeighPro
+* Author Weis Electonics Pvt. Ltd.
+* Contribution by emdroidery
+*/
 package in.weighpro.listComponents;
 
 import java.awt.Color;
@@ -12,13 +21,14 @@ public class CustomList extends JList<Object> {
 	 */
 	private static final long serialVersionUID = -8292150763793513858L;
 
+	//constructor to initialize the list
 	public CustomList(Object[] items) {
-		super(items);
-		setOpaque(false);
-		setForeground(new Color(255, 255, 255, 255));
-		setFont(new Font(Font.DIALOG, Font.PLAIN, 18));
-		this.setCellRenderer(new CustomListLabel());
-		setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		super(items);//populate the list with items passed
+		setOpaque(false);//make the list transparent
+		setForeground(new Color(255, 255, 255, 255));//set the text color to white
+		setFont(new Font(Font.DIALOG, Font.PLAIN, 18));//set the font of the list items
+		this.setCellRenderer(new CustomListLabel());//custom cell renderer used to render the contents of the list
+		setSelectionMode(ListSelectionModel.SINGLE_SELECTION);//allow only single selection
 
 	}
 
