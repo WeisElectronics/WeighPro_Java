e804738fac5e9d089073bd2f7f98893186a4dad8
rasheedf <rasheedf@IN-HYD2-567QQ12.ant.amazon.com>
2014-07-24 15:36:56 +0530

    Added comments to DataIndexQuery.java

--- WeighPro/src/in/weighpro/databaseQueryHandler/DataIndexQuery.java
+++ WeighPro/src/in/weighpro/databaseQueryHandler/DataIndexQuery.java
@@ -1,3 +1,13 @@
+/*
+* This class provides the database query
+* used to get the value corresponding to 
+* the given index from the database
+
+* Made for WeighPro
+* Author Weis Electonics Pvt. Ltd.
+* Contribution by emdroidery
+*/
+
 package in.weighpro.databaseQueryHandler;
 
 import in.weighpro.databaseConstructs.DatabaseConnection;
@@ -10,18 +20,22 @@ public class DataIndexQuery {
 
 	private String value;
 
-	public DataIndexQuery(String type, int index) throws SQLException {
-		String queryString = "SELECT value FROM " + type + " WHERE ID=?";
-		DatabaseConnection connection = new DatabaseConnection();
+	//constructor to initialize the database connection and execute the query
+	public DataIndexQuery(String type, int index) throws SQLException {//Takes the type and index as the arguments
+																		//can throw an SQL Exception due to the database handling
+		String queryString = "SELECT value FROM " + type + " WHERE ID=?";//creating the database query
+		DatabaseConnection connection = new DatabaseConnection();//initializing the connection
 		PreparedStatement prepStat = connection.getConnection()
-				.prepareStatement(queryString);
-		prepStat.setInt(1, index);
-		ResultSet rs = prepStat.executeQuery();
+				.prepareStatement(queryString);//initializing the prepared statement that will be used to execute the query
+		prepStat.setInt(1, index);//inserting the index in the prepared statement
+		ResultSet rs = prepStat.executeQuery();//Result set to store the retrieved values
 		if (rs.next())
-			value = rs.getString(1);
+			value = rs.getString(1);//getting the retrieved value
 
 	}
 
+	
+	//method to get the value of the data type corresponding to the index
 	public String getValue() {
 		return this.value;
 	}
