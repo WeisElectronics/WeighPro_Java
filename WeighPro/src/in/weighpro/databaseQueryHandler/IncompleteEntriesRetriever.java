0c106e2f6f58ac1171447ab8f97d3b0b3c9341f1
rasheedf <rasheedf@IN-HYD2-567QQ12.ant.amazon.com>
2014-07-24 16:02:44 +0530

    Added comments to IncompleteEntriesRetriever.java,InsertEntry.java

--- WeighPro/src/in/weighpro/databaseQueryHandler/IncompleteEntriesRetriever.java
+++ WeighPro/src/in/weighpro/databaseQueryHandler/IncompleteEntriesRetriever.java
@@ -1,3 +1,13 @@
+/*
+* This class provides the database query
+* used to get the incomplete entries
+*  from the database
+
+* Made for WeighPro
+* Author Weis Electonics Pvt. Ltd.
+* Contribution by emdroidery
+*/
+
 package in.weighpro.databaseQueryHandler;
 
 import in.weighpro.databaseConstructs.DatabaseConnection;
@@ -9,23 +19,25 @@ import java.sql.SQLException;
 public class IncompleteEntriesRetriever {
 	int[] incompleteIndices;
 
-	public IncompleteEntriesRetriever() throws SQLException {
-		String queryString = "SELECT ID FROM entries WHERE status=?";
-		DatabaseConnection connection = new DatabaseConnection();
+	//Constructor to initialize the database connection and retrieve the incomplete entries
+	public IncompleteEntriesRetriever() throws SQLException {//may throw an SQL Exception because of database handling
+		String queryString = "SELECT ID FROM entries WHERE status=?";//Creating the database query
+		DatabaseConnection connection = new DatabaseConnection();//initializing the database connection
 		PreparedStatement prepStat = connection.getConnection()
-				.prepareStatement(queryString);
-		prepStat.setBoolean(1, false);
-		ResultSet rs = prepStat.executeQuery();
-		rs.last();
-		incompleteIndices = new int[rs.getRow()];
-		rs.beforeFirst();
+				.prepareStatement(queryString);//Initializing the prepared statement to execute the query
+		prepStat.setBoolean(1, false);//to get entries where status is false(incomplete)
+		ResultSet rs = prepStat.executeQuery();//Result set to store the retrieved entries
+		rs.last();//move to the last row to get the count
+		incompleteIndices = new int[rs.getRow()];//initializing an array to store the incomplete entries
+		rs.beforeFirst();//move to the row before the first row
 		int i = 0;
-		while(rs.next()) {
-			incompleteIndices[i++] = rs.getInt(1);
+		while(rs.next()) {//iterating through the result set to store the entries in the array
+			incompleteIndices[i++] = rs.getInt(1);//getting the indices of the incomplete entries
 		}
 
 	}
 	
+	//method to get the array containing the indices of the incomplete entries
 	public int[] getIncompleteEntries(){
 		return this.incompleteIndices;
 	}
--- WeighPro/src/in/weighpro/databaseQueryHandler/InsertEntryQuery.java
+++ WeighPro/src/in/weighpro/databaseQueryHandler/InsertEntryQuery.java
@@ -1,3 +1,13 @@
+/*
+* This class provides the database query
+* used to insert an entry 
+* in the database
+
+* Made for WeighPro
+* Author Weis Electonics Pvt. Ltd.
+* Contribution by emdroidery
+*/
+
 package in.weighpro.databaseQueryHandler;
 
 import in.weighpro.dataComponents.Customer;
@@ -14,34 +24,39 @@ import java.sql.SQLException;
 public class InsertEntryQuery {
 	private int index;
 
+	//constructor to initialize the database connection and execute the insert in the database
 	public InsertEntryQuery(Vehicle vehicle, Customer customer,
 			Material material, int amount, Weight weight, TimeDetails time)
-			throws SQLException {
+			throws SQLException {//Takes vehicle,customer,material,amount,weight,and time objects as the arguments
+		//May throw an SQL Exception due to the database handling
+		//creating the query to insert the data in the database
 		String query = "INSERT INTO entries (vehicleID,customerID,materialID,tareWeight,grossWeight,netWeight,amount,entryTimeStamp,exitTimeStamp,status,incomingType)VALUES(?,?,?,?,?,?,?,?,?,?,?)";
-		DatabaseConnection connection = new DatabaseConnection();
+		DatabaseConnection connection = new DatabaseConnection();//Initializing the database connection 
 		PreparedStatement prepStat = connection.getConnection()
-				.prepareStatement(query);
-		prepStat.setInt(1, vehicle.getIndex());
-		prepStat.setInt(2, customer.getIndex());
-		prepStat.setInt(3, material.getIndex());
-		prepStat.setInt(4, weight.getTareWeight());
-		prepStat.setInt(5, weight.getGrossWeight());
-		prepStat.setInt(6, weight.getNetWeight());
-		prepStat.setInt(7, amount);
-		prepStat.setString(8, time.getEntryTimeStamp());
-		prepStat.setString(9, time.getExitTimeStamp());
-		prepStat.setBoolean(10, false);
-		prepStat.setBoolean(11, weight.getIncomingType());
-		prepStat.executeUpdate();
+				.prepareStatement(query);//initialing the prepared statement that will be used to execute the query
+		prepStat.setInt(1, vehicle.getIndex());//setting the vehicle index
+		prepStat.setInt(2, customer.getIndex());//setting the customer index
+		prepStat.setInt(3, material.getIndex());//setting the material index
+		prepStat.setInt(4, weight.getTareWeight());//setting the tare weight
+		prepStat.setInt(5, weight.getGrossWeight());//setting the gross weight
+		prepStat.setInt(6, weight.getNetWeight());//setting the net weight
+		prepStat.setInt(7, amount);//setting the amount
+		prepStat.setString(8, time.getEntryTimeStamp());//setting the entry time stamp
+		prepStat.setString(9, time.getExitTimeStamp());//setting the exit time stamp
+		prepStat.setBoolean(10, false);//setting the status
+		prepStat.setBoolean(11, weight.getIncomingType());//setting the incoming type
+		prepStat.executeUpdate();//update the database
 
-		query = "SELECT ID FROM entries WHERE entryTimeStamp = ?";
-		prepStat = connection.getConnection().prepareStatement(query);
-		prepStat.setString(1, time.getEntryTimeStamp());
-		ResultSet rs = prepStat.executeQuery();
+		query = "SELECT ID FROM entries WHERE entryTimeStamp = ?";//getting the id of the inserted entry
+		prepStat = connection.getConnection().prepareStatement(query);//initializing the prepared statement that will be used to retrieve the id 
+		prepStat.setString(1, time.getEntryTimeStamp());//the query has the timestamp as the criteria for selection
+		ResultSet rs = prepStat.executeQuery();//result set to store the retrieved entries
 		if (rs.next())
-			index = rs.getInt(1);
+			index = rs.getInt(1);//getting the index from the retrieved data stored in the result set
 	}
 
+	
+	//method to get the index of the inserted entry
 	public int getIndex() {
 		return index;
 	}
